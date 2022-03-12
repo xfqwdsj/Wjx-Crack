92df6359799a997dff01a8ab06e4335c931b4a12
HMLTFan <xfqwdsj@qq.com>
2022-03-12 14:22:29 +0800

    .

--- jqmobo2.js
+++ jqmobo2.js
@@ -1,4 +1,4 @@
-(function() { window.processMinMax = function() {} })()
+function processMinMax() {}
 
 function setCookie(a, b, c, d, e, f) {
     document.cookie = a + "=" + encodeURIComponent(b) + (c ? "; expires=" + c : "") + (d ? "; path=" + d : "") + (e ? "; domain=" + e : "") + (f ? "; secure" : "")
